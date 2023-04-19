d55efa684d949dbec140af641df604495dad293a
Edward Glöckner <101050443+EdwardGlockner@users.noreply.github.com>
2023-04-19 15:12:00 +0200

    new keymaps, splits

--- lua/edward/init.lua
+++ lua/edward/init.lua
@@ -1,1 +1,2 @@
 require("edward.remap")
+
--- lua/edward/remap.lua
+++ lua/edward/remap.lua
@@ -1,3 +1,5 @@
 
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>v", vim.cmd.vsp)
+vim.keymap.set("n", "<leader>h", vim.cmd.sp)
