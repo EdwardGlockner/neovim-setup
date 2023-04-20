96cf8ac7906db485f53cec85cc931177406f5aa6
Edward Glöckner <101050443+EdwardGlockner@users.noreply.github.com>
2023-04-20 11:22:32 +0200

    fixing splitting remaps

--- lua/edward/remap.lua
+++ lua/edward/remap.lua
@@ -4,5 +4,5 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 -- Handles splitting
 vim.keymap.set("n", "<leader>v", vim.cmd.vsp)
 vim.keymap.set("n", "<leader>h", vim.cmd.sp)
-vim.keymap.set("n", "<leader>x", vim.cmd.FocusToggle)
-vim.keymap.set("n", "<leader>m", vim.cmd.FocusMaximise)
+vim.keymap.set("n", "<leader>e", vim.cmd.FocusToggle)
+vim.keymap.set("n", "<leader>t", vim.cmd.FocusMaxOrEqual)
